6e34fd32cc0b9146ea1c3b0bb3fc1c2f13a2c29a
wentaol6 <wentaol6@student.unimelb.edu.au>
2024-07-25 22:58:53 +1000

    Add authentication for create delete and edit

--- Controllers/JokeController.cs
+++ Controllers/JokeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JokesWebApp.Data;
 using JokesWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JokesWebApp.Controllers
 {
@@ -56,6 +57,7 @@ namespace JokesWebApp.Controllers
         }
 
         // GET: Joke/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -64,6 +66,7 @@ namespace JokesWebApp.Controllers
         // POST: Joke/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -78,6 +81,7 @@ namespace JokesWebApp.Controllers
         }
 
         // GET: Joke/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -129,6 +133,7 @@ namespace JokesWebApp.Controllers
         }
 
         // GET: Joke/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -147,6 +152,7 @@ namespace JokesWebApp.Controllers
         }
 
         // POST: Joke/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
